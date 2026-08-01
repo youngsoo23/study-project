da1df52ec897927fc6a91aa657d4b61ca60dfa19
youngsoo23 <oh.youngsoo23@gmail.com>
2026-08-01 22:56:27 +0900

    first commit

---
+++ src/test/java/com/young/studyproject/StudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.studyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
